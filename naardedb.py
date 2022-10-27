a74d0db83a694cf17552efed052965a6037e730b
FelixvL <digitaleservice@gmail.com>
2022-10-27 16:29:11 +0200

    mijn eerste photo

---
+++ naardedb.py
@@ -0,0 +1,31 @@
+import mariadb
+
+print("hoi")
+print("we gaan verder")
+
+# Assignment
+#   =
+
+# Arithmyc
+# - + / *   **
+
+
+# Concateneren
+# +
+
+# Comparitive
+# == < > !=  <=
+
+# Logische
+# And   &&
+
+
+getal1 = 15
+getal2 = 4
+
+print(getal1 != 7  & getal2 == 4)
+woord1 = int(input("hoe oud ben je"))
+woord2 = int(input("overhoeveel jaar"))
+
+
+print(woord1 + woord2)
---
+++ naarmssqldb.py
@@ -0,0 +1,12 @@
+import pyodbc 
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-CMG3OIG\SQLEXPRESS;'
+                      'Database=TSQL;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute("SELECT sc.companyname, country city FROM Sales.Customers as sc WHERE sc.country = 'Mexico'")
+
+for i in cursor:
+    print(i)
